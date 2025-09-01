8287e9e7fb54dabd647af60d54276ce4e1bb9571
zhangwei920211 <834675966@qq.com>
2025-09-01 22:48:48 +0800

    修改_gen2

---
+++ hooks/pre.ps1
@@ -0,0 +1,4 @@
+# PowerShell pre hook for cargo-generate
+# Print an initializing message in PowerShell-friendly way.
+
+Write-Output "Initializing Rust project template..."
---
+++ hooks/pre.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# POSIX-compatible pre hook for cargo-generate
+# Prints an initializing message in a cross-shell-safe way.
+
+printf "Initializing Rust project template...\n"
