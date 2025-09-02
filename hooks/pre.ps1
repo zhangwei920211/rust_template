e70ee12c6ccc1742388f8162585e2005c2082d5b
zhangwei <834675966@qq.com>
2025-09-02 11:18:45 +0800

    Delete hooks directory
    删除 hooks

--- hooks/pre.ps1
+++
@@ -1,4 +0,0 @@
-# PowerShell pre hook for cargo-generate
-# Print an initializing message in PowerShell-friendly way.
-
-Write-Output "Initializing Rust project template..."
--- hooks/pre.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# POSIX-compatible pre hook for cargo-generate
-# Prints an initializing message in a cross-shell-safe way.
-
-printf "Initializing Rust project template...\n"
